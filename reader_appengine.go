f621bd8462ffa0559bd109551d51b8c9648c17c6
Ben Cox <Ben@Benjojo.co.uk>
2015-03-19 21:12:49 +0000

    Added the ability to open gzip'd maximind databases
    This is mainly added because google app engine does not allow you to
    upload massive files or services that have limits on the size of file
    you can deploy to the file system, in this case my issue was with
    google appengine

--- reader_appengine.go
+++ reader_appengine.go
@@ -2,7 +2,10 @@
 
 package maxminddb
 
-import "io/ioutil"
+import (
+	"io/ioutil"
+	"os"
+)
 
 // Open takes a string path to a MaxMind DB file and returns a Reader
 // structure or an error. The database file is opened using a memory map,
@@ -11,6 +14,32 @@ import "io/ioutil"
 // the resources to the system.
 func Open(file string) (*Reader, error) {
 	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return FromBytes(bytes)
+}
+
+// OpenGzip takes a string path to a MaxMind DB file that is gziped and returns
+// a Reader structure or an error. The database file is opened using a memory
+// map,except on Google App Engine where mmap is not supported; there the
+// database is loaded into memory. Use the Close method on the Reader object
+// to return the resources to the system.
+func OpenGzip(file string) (*Reader, error) {
+	filehandle, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer filehandle.Close()
+
+	gziphandle, err := gzip.NewReader(filehandle)
+	if err != nil {
+		return nil, err
+	}
+	defer gziphandle.Close()
+
+	bytes, err := ioutil.ReadAll(gziphandle)
 	if err != nil {
 		return nil, err
 	}
